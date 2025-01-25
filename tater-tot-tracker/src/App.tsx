d6dd764d0730b2a0be7fbfa05c91201d86cf47e7
EDJ <emdjjohnson@gmail.com>
2025-01-25 14:32:45 -0700

    Read Potato values

--- tater-tot-tracker/src/App.tsx
+++ tater-tot-tracker/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import babyData from './babyData.json';
+import potatoData from './potatoData.json'; 
+
 
 interface FetusSize {
   weeks: number;
@@ -14,30 +16,14 @@ const App: React.FC = () => {
     sizeInPotatoes: "no potatoes :(", //Initalized value is silly, bc why not
   });
 
-
-  // Potato Sizes
-  const averageGramsRussetPotato = 173;
-  const averageGramsHashBrown = 60;
-  const averageGramsRedPotato = 148;
-  const gramsMashedPotato = 1;
-
   // Calculate the number of potatoes based on the week and potato unit
   const calculatePotatos = (week: number, potatoUnit: string) => {
     const babyDataForWeek = babyData.find((data) => data.week === week);
     if (!babyDataForWeek) return 0;
-  
-    switch (potatoUnit) {
-      case 'Russet Potato':
-        return babyDataForWeek.weight / averageGramsRussetPotato;
-      case 'Hash Brown':
-        return babyDataForWeek.weight / averageGramsHashBrown;
-      case 'Red Potato':
-        return babyDataForWeek.weight / averageGramsRedPotato;
-      case 'Grams of Mashed Potato':
-          return babyDataForWeek.weight * gramsMashedPotato;
-      default:
-        return 0;
-    }
+    const potatoInfo = potatoData.find((potato) => potato.displayName === potatoUnit);
+    if (!potatoInfo) return 0;
+    const potatoValue = babyDataForWeek.weight / potatoInfo.avgGrams;
+    return potatoValue.toFixed(2);
   };
 
   // Get the potato output string based on the week and potato unit
@@ -46,7 +32,9 @@ const App: React.FC = () => {
     if (potatoValue === 0) {
       return 'Unknown size';
     }
-    else{return `${potatoValue} ${potatoUnit}`;}
+    const potatoInfo = potatoData.find((potato) => potato.displayName === potatoUnit);
+    if (!potatoInfo) return 0;
+    else{return `${potatoValue} ${potatoInfo.pluralName}`;}
   }
 
   const handleWeekChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -71,32 +59,14 @@ const App: React.FC = () => {
       <h1>Tater Tot Tracker</h1>
 
       {/* Potato Unit Buttons */}
-      <div>
+      {potatoData.map((potato) => (
         <button
-          onClick={() => handlePotatoUnitChange('Russet Potato')}
-          className={`p-2 m-2 border rounded ${selectedPotatoUnit === 'Russet Potato' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handlePotatoUnitChange(potato.displayName)}
+          className={`p-2 m-2 border rounded ${selectedPotatoUnit === potato.displayName ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
         >
-          Russet Potato
+          {potato.displayName}
         </button>
-        <button
-          onClick={() => handlePotatoUnitChange('Hash Brown')}
-          className={`p-2 m-2 border rounded ${selectedPotatoUnit === 'Hash Brown' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-        >
-          Hash Brown
-        </button>
-        <button
-          onClick={() => handlePotatoUnitChange('Red Potato')}
-          className={`p-2 m-2 border rounded ${selectedPotatoUnit === 'Red Potato' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-        >
-          Red Potato
-        </button>
-        <button
-          onClick={() => handlePotatoUnitChange('Grams of Mashed Potato')}
-          className={`p-2 m-2 border rounded ${selectedPotatoUnit === 'Grams of Mashed Potato' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-        >
-          Mashed Potato
-        </button>
-      </div>
+      ))}
 
       {/* Week Input */}
       <div>
